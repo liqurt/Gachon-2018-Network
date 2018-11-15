5dbe5562f862c034e66d8bd19cc877f792a63e24
윤승일(Seung-Il Yoon) <liqurt@naver.com>
2018-11-15 20:35:59 +0900

    Update IndianPoker_Server.java

--- Network_IndianPoker/src/IndianPoker_Server.java
+++ Network_IndianPoker/src/IndianPoker_Server.java
@@ -2,10 +2,22 @@ import java.util.*;
 
 public class IndianPoker_Server {
 	
-	static int game; //현재 게임
+	int pandon;
+	static int game=0; //NOW GAME
 	
 	public int getNowGame() {
 		return game;
 	}
 	
+	public void countUpGame() {
+		game++;
+	}
+
+	public int getPandon() {
+		return pandon;
+	}
+	
+	public void setPandon(int left, int right) {
+		pandon = left+right+pandon;
+	}
 }
